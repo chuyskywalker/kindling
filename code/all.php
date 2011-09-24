e6bd808e21b5d31cde90f52e30a88d3114180475
Jeff Minard <j@jrm.cc>
2011-09-24 11:09:29 -0700

    ignore config

--- code/all.php
+++ code/all.php
@@ -4,11 +4,7 @@ require __DIR__ . '/../config.php';
 
 define('VIEWDIR', __DIR__ . '/views/');
 
-//require __DIR__ . '/models/SuperTable.php';
-//require __DIR__ . '/models/Updates.php';
-//require __DIR__ . '/models/History.php';
-//require __DIR__ . '/models/Apps.php';
-//require __DIR__ . '/models/Users.php';
-//require __DIR__ . '/models/Watches.php';
+require __DIR__ . '/models/Item.php';
+require __DIR__ . '/libs/redisclient.php';
 require __DIR__ . '/klein.php';
 require __DIR__ . '/util.php';
