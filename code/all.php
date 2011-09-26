77820a777bd201ed9a5a29bba3b9a13e55113e97
Jeff Minard <j@jrm.cc>
2011-09-25 23:24:54 -0700

    Add a lovely pagination system

--- code/all.php
+++ code/all.php
@@ -13,5 +13,6 @@ require __DIR__ . '/models/image.php';
 require __DIR__ . '/models/status.php';
 require __DIR__ . '/libs/redisclient.php';
 require __DIR__ . '/libs/Form.php';
+require __DIR__ . '/libs/paginate.php';
 require __DIR__ . '/klein.php';
 require __DIR__ . '/util.php';
