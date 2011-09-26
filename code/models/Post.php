93b806d9df0d8341b39e08af681ed6299631f3a0
Jeff Minard <j@jrm.cc>
2011-09-25 18:31:54 -0700

    Add labels to post type fields Hide h2/nav on edit page when from bm Add "make new X" nav on edit page Hide "delete" for non-editing situations Keep BM attribute when submitting with errors

--- code/models/Post.php
+++ code/models/Post.php
@@ -5,8 +5,8 @@ class Item_post extends Item{
     const TYPE = 'post';
 
     static $fields = array(
-          array('id' => 'title', 'type' => Form::TYPE_TEXT, 'rules' => array(Form::RULE_REQUIRED => ''))
-        , array('id' => 'content', 'type' => Form::TYPE_LONGTEXT, 'rules' => array(Form::RULE_REQUIRED => ''))
+          array('id' => 'title', 'label' => 'Title', 'type' => Form::TYPE_TEXT, 'rules' => array(Form::RULE_REQUIRED => ''))
+        , array('id' => 'content', 'label' => 'Post Content', 'type' => Form::TYPE_LONGTEXT, 'rules' => array(Form::RULE_REQUIRED => ''))
     );
 
 }
--- code/models/audio.php
+++ code/models/audio.php
@@ -5,9 +5,9 @@ class Item_audio extends Item{
     const TYPE = 'audio';
 
     static $fields = array(
-          array('id' => 'title', 'type' => Form::TYPE_TEXT, 'rules' => array(Form::RULE_REQUIRED => ''))
-        , array('id' => 'url', 'type' => Form::TYPE_URL, 'rules' => array(Form::RULE_REQUIRED => '', Form::RULE_URL_AUDIO => ''))
-        , array('id' => 'comment', 'type' => Form::TYPE_LONGTEXT, 'rules' => array())
+          array('id' => 'title', 'label' => 'Title', 'type' => Form::TYPE_TEXT, 'rules' => array(Form::RULE_REQUIRED => ''))
+        , array('id' => 'url', 'label' => 'MP3 URL', 'type' => Form::TYPE_URL, 'rules' => array(Form::RULE_REQUIRED => '', Form::RULE_URL_AUDIO => ''))
+        , array('id' => 'comment', 'label' => 'Comment', 'type' => Form::TYPE_LONGTEXT, 'rules' => array())
     );
 
 }
