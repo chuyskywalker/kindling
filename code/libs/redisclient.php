16e275b7f74f041066ce028fc35691a6de6beb6d
Jeff Minard <j@jrm.cc>
2011-09-27 00:01:10 -0700

    Utilize defines for connection Switch to specific DB if requested

--- code/libs/redisclient.php
+++ code/libs/redisclient.php
@@ -13,7 +13,14 @@ class rc {
     static function get() {
         if (!isset(self::$redis)) {
             self::$redis = new Redis();
-            self::$redis->connect('localhost', 6379, 5);
+            //self::$redis->connect(REDIS_HOST, 6379, 5);
+            $h = defined('REDIS_HOST') ? REDIS_HOST : 'localhost';
+            $p = defined('REDIS_PORT') ? REDIS_PORT : 6379;
+            $t = defined('REDIS_CONNECT_TIMEOUT') ? REDIS_CONNECT_TIMEOUT : 5;
+            self::$redis->connect($h, $p, $t);
+            if (defined('REDIS_DBID')) {
+                self::$redis->select(REDIS_DBID);
+            }
         }
         return self::$redis;
     }
